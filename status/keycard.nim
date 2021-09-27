b52edb129c14917a14cae3e3d5244491f0e347ad
Andrea Franz <andrea@gravityblast.com>
2021-09-27 13:28:53 +0200

    fix import keycard_go

--- status/keycard.nim
+++ status/keycard.nim
@@ -1,21 +1,11 @@
 import keycard_go
 
-import ./types/[bookmark]
-
 type
     KeycardModel* = ref object
 
 proc newKeycardModel*(): KeycardModel =
   result = KeycardModel()
 
-# proc storeBookmark*(self: BrowserModel, url: string, name: string): Bookmark =
-#   result = status_browser.storeBookmark(url, name)
+# proc storeBookmark*(self: KeycardModel) =
+#   result = keycard_go.Select()
 
-# proc updateBookmark*(self: BrowserModel, ogUrl: string, url: string, name: string) =
-#   status_browser.updateBookmark(ogUrl, url, name)
-
-# proc getBookmarks*(self: BrowserModel): string =
-#   result = status_browser.getBookmarks()
-
-# proc deleteBookmark*(self: BrowserModel, url: string) =
-#   status_browser.deleteBookmark(url)
