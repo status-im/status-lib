c07e04e976f46edc72077b4095001afd10170a6a
Richard Ramos <info@richardramos.me>
2021-09-28 14:41:40 -0400

    feat: mailserver request signals (#46)

--- status/signals/mailserver.nim
+++ status/signals/mailserver.nim
@@ -13,6 +13,13 @@ type MailserverRequestCompletedSignal* = ref object of Signal
 type MailserverRequestExpiredSignal* = ref object of Signal
   # TODO
 
+type HistoryRequestStartedSignal* = ref object of Signal
+type HistoryRequestCompletedSignal* = ref object of Signal
+type HistoryRequestFailedSignal* = ref object of Signal
+  errorMessage*: string
+  error*: bool
+
+
 proc fromEvent*(T: type MailserverRequestCompletedSignal, jsonSignal: JsonNode): MailserverRequestCompletedSignal = 
   result = MailserverRequestCompletedSignal()
   result.signalType = SignalType.MailserverRequestCompleted
@@ -27,3 +34,18 @@ proc fromEvent*(T: type MailserverRequestExpiredSignal, jsonSignal: JsonNode): M
   # TODO: parse signal
   result = MailserverRequestExpiredSignal()
   result.signalType = SignalType.MailserverRequestExpired
+
+proc fromEvent*(T: type HistoryRequestStartedSignal, jsonSignal: JsonNode): HistoryRequestStartedSignal = 
+  result = HistoryRequestStartedSignal()
+  result.signalType = SignalType.HistoryRequestStarted
+
+proc fromEvent*(T: type HistoryRequestCompletedSignal, jsonSignal: JsonNode): HistoryRequestCompletedSignal = 
+  result = HistoryRequestCompletedSignal()
+  result.signalType = SignalType.HistoryRequestCompleted
+
+proc fromEvent*(T: type HistoryRequestFailedSignal, jsonSignal: JsonNode): HistoryRequestFailedSignal = 
+  result = HistoryRequestFailedSignal()
+  result.signalType = SignalType.HistoryRequestStarted
+  if jsonSignal["event"].kind != JNull:
+    result.errorMessage = jsonSignal["event"]{"errorMessage"}.getStr()
+    result.error = result.errorMessage != ""
