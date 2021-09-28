7999a23acc6e5a8fffdce71be5de494f146c9140
Anthony Laibe <anthony@laibe.cc>
2021-09-28 10:53:25 +0200

    refactor: browser use object

--- status/statusgo_backend/browser.nim
+++ status/statusgo_backend/browser.nim
@@ -1,26 +1,34 @@
-import core, ../types/[bookmark], json, chronicles
+import json, chronicles
+import core
+import ../types/[bookmark]
 
-proc storeBookmark*(url: string, name: string): Bookmark =
-  let payload = %* [{"url": url, "name": name}]
-  result = Bookmark(name: name, url: url)
+proc storeBookmark*(bookmark: Bookmark): Bookmark =
+  let payload = %* [{"url": bookmark.url, "name": bookmark.name}]
   try:
     let resp = callPrivateRPC("browsers_storeBookmark", payload).parseJson["result"]
-    result.imageUrl = resp["imageUrl"].getStr
+    bookmark.imageUrl = resp["imageUrl"].getStr
+    return bookmark
   except Exception as e:
     error "Error updating bookmark", msg = e.msg
     discard
 
-proc updateBookmark*(ogUrl: string, url: string, name: string) =
-  let payload = %* [ogUrl, {"url": url, "name": name}]
+proc updateBookmark*(originalUrl: string, bookmark: Bookmark) =
+  let payload = %* [originalUrl, {"url": bookmark.url, "name": bookmark.name}]
   try:
     discard callPrivateRPC("browsers_updateBookmark", payload)
   except Exception as e:
     error "Error updating bookmark", msg = e.msg
     discard
 
-proc getBookmarks*(): string =
+proc getBookmarks*(): seq[Bookmark] =
   let payload = %* []
-  result = callPrivateRPC("browsers_getBookmarks", payload)
+  try:
+    let responseResult = callPrivateRPC("browsers_getBookmarks", payload).parseJson["result"]
+    if responseResult.kind != JNull:
+      for bookmark in responseResult:
+        result.add(Bookmark(url: bookmark{"url"}.getStr, name: bookmark{"name"}.getStr, imageUrl: bookmark{"imageUrl"}.getStr))
+  except:
+    discard
 
 proc deleteBookmark*(url: string) =
   let payload = %* [url]
