70dbbab546b96dd3d7836ff7ae5e4a27e7e983a0
Anthony Laibe <anthony@laibe.cc>
2021-10-21 14:39:58 +0200

    refactor: add save account

--- status/statusgo_backend_new/accounts.nim
+++ status/statusgo_backend_new/accounts.nim
@@ -1,4 +1,4 @@
-import json, json_serialization, chronicles
+import json, json_serialization, chronicles, nimcrypto
 import ./core, ./utils
 import ./response_type
 
@@ -12,8 +12,16 @@ logScope:
 const NUMBER_OF_ADDRESSES_TO_GENERATE = 5
 const MNEMONIC_PHRASE_LENGTH = 12
 
+const GENERATED* = "generated"
+const SEED* = "seed"
+const KEY* = "key"
+const WATCH* = "watch"
+
 proc getAccounts*(): RpcResponse[JsonNode] {.raises: [Exception].} =
   return core.callPrivateRPC("accounts_getAccounts")
+
+proc deleteAccount*(address: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  return core.callPrivateRPC("accounts_deleteAccount", %* [address])
 
 proc generateAddresses*(paths: seq[string]): RpcResponse[JsonNode] {.raises: [Exception].} =
   let payload = %* {
@@ -101,6 +109,54 @@ proc storeDerivedAccounts*(id, hashedPassword: string, paths: seq[string]):
   except RpcException as e:
     error "error doing rpc request", methodName = "storeDerivedAccounts", exception=e.msg
     raise newException(RpcException, e.msg)
+
+proc storeAccounts*(id, hashedPassword: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* {
+    "accountID": id,
+    "password": hashedPassword
+  }
+
+  try:
+    let response = status_go.multiAccountStoreAccount($payload)
+    result.result = Json.decode(response, JsonNode)
+
+  except RpcException as e:
+    error "error doing rpc request", methodName = "storeAccounts", exception=e.msg
+    raise newException(RpcException, e.msg)
+
+proc hashPassword*(password: string): string =
+  result = "0x" & $keccak_256.digest(password)
+
+proc saveAccount*(
+  address: string,
+  name: string,
+  password: string,
+  color: string,
+  accountType: string,
+  isADerivedAccount = true,
+  walletIndex: int = 0,
+  id: string = "",
+  publicKey: string = "",
+) {.raises: [Exception].} =
+  var derivationPath = "m/44'/60'/0'/0/0"
+  let hashedPassword = hashPassword(password)
+
+  if (isADerivedAccount):
+    let derivationPath = (if accountType == GENERATED: "m/" else: "m/44'/60'/0'/0/") & $walletIndex
+    discard storeDerivedAccounts(id, hashedPassword, @[derivationPath])
+  elif accountType == KEY:
+    discard storeAccounts(id, hashedPassword)
+
+  discard callPrivateRPC("accounts_saveAccounts", %* [
+    [{
+      "color": color,
+      "name": name,
+      "address": address,
+      "public-key": publicKey,
+      "type": accountType,
+      "path": derivationPath
+    }]
+  ])
 
 proc addPeer*(peer: string): RpcResponse[JsonNode] {.raises: [Exception].} =
   try:
