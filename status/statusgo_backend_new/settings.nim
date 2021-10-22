e23c9e520c6d846a7a8f50a904167efe9ec4c1e7
Anthony Laibe <anthony@laibe.cc>
2021-10-22 11:36:28 +0200

    refactor: save settings

--- status/statusgo_backend_new/settings.nim
+++ status/statusgo_backend_new/settings.nim
@@ -5,3 +5,6 @@ export response_type
 
 proc getSettings*(): RpcResponse[JsonNode] {.raises: [Exception].} =
   return core.callPrivateRPC("settings_getSettings")
+
+proc saveSettings*(key: string, value: string | JsonNode | bool | int) {.raises: [Exception].} =
+  discard core.callPrivateRPC("settings_saveSetting", %* [key, value])
