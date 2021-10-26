dfd0cb78224835bf36d53b22f3a4ef64be38b09a
Jonathan Rainville <rainville.jonathan@gmail.com>
2021-10-26 09:19:13 -0400

    add getTransfersByAddress (#93)

--- status/statusgo_backend_new/transactions.nim
+++ status/statusgo_backend_new/transactions.nim
@@ -1,7 +1,17 @@
-import json
+import json, stint, chronicles, strutils, conversions
 
-import ./core
+
+import ../types/transaction
+import ./core as core
 
 proc checkRecentHistory*(addresses: seq[string]) {.raises: [Exception].} =
   let payload = %* [addresses]
   discard callPrivateRPC("wallet_checkRecentHistory", payload)
+
+proc getTransfersByAddress*(address: string, toBlock: Uint256, limit: int, loadMore: bool = false): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let
+    toBlockParsed = if not loadMore: newJNull() else: %("0x" & stint.toHex(toBlock))
+    limitParsed = "0x" & limit.toHex.stripLeadingZeros
+    
+  callPrivateRPC("wallet_getTransfersByAddress", %* [address, toBlockParsed, limitParsed, loadMore])
+    
