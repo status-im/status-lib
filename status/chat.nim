ffe6d5e0f0fbc699c931bdfbc72ddc42d8201251
r4bbit.eth <445106+PascalPrecht@users.noreply.github.com>
2021-11-16 12:37:56 +0100

    feat(chat): add bindings to `markActivityCenterNotificationsUnread` API (#53)
    This is needed so clients can implement the functionality of marking notifications
    as unread.

--- status/chat.nim
+++ status/chat.nim
@@ -74,6 +74,11 @@ type
     channelId*: string
     notificationTypes*: seq[ActivityCenterNotificationType]
 
+  MarkAsUnreadNotificationProperties* = ref object of Args
+    communityId*: string
+    channelId*: string
+    notificationTypes*: seq[ActivityCenterNotificationType]
+
 type ChatModel* = ref object
   publicKey*: string
   events*: EventEmitter
@@ -614,6 +619,16 @@ markAsReadProps: MarkAsReadNotificationProperties): string =
     result = e.msg
   
   self.events.emit("markNotificationsAsRead", markAsReadProps)
+
+proc markActivityCenterNotificationUnread*(self: ChatModel, notificationId: string,
+markAsUnreadProps: MarkAsUnreadNotificationProperties): string =
+  try:
+    status_chat.markActivityCenterNotificationsUnread(@[notificationId])
+  except Exception as e:
+    error "Error marking as unread", msg = e.msg
+    result = e.msg
+  
+  self.events.emit("markNotificationsAsUnread", markAsUnreadProps)
 
 proc acceptActivityCenterNotifications*(self: ChatModel, ids: seq[string]): string =
   try:
