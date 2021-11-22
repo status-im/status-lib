e1ba0336332fa1d614ab1eaa44037f6030b210e0
Sale Djenic <aleksandardjenic@status.im>
2021-11-22 19:36:48 +0100

    refactor: settings updated

--- status/statusgo_backend_new/settings.nim
+++ status/statusgo_backend_new/settings.nim
@@ -6,5 +6,6 @@ export response_type
 proc getSettings*(): RpcResponse[JsonNode] {.raises: [Exception].} =
   return core.callPrivateRPC("settings_getSettings")
 
-proc saveSettings*(key: string, value: string | JsonNode | bool | int) {.raises: [Exception].} =
-  discard core.callPrivateRPC("settings_saveSetting", %* [key, value])
+proc saveSettings*(key: string, value: string | JsonNode | bool | int): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [key, value]
+  result = core.callPrivateRPC("settings_saveSetting", payload)
