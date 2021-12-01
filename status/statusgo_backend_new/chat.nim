0d2aa3797bbe28be2a2613d953967b74c82ca423
Sale Djenic <aleksandardjenic@status.im>
2021-12-01 17:45:37 +0100

    feat(chat): a method for creating public chat is added

--- status/statusgo_backend_new/chat.nim
+++ status/statusgo_backend_new/chat.nim
@@ -7,3 +7,7 @@ export response_type
 proc getChats*(): RpcResponse[JsonNode] {.raises: [Exception].} =
   let payload = %* []
   result = callPrivateRPC("chats".prefix, payload)
+
+proc createPublicChat*(chatId: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [{"ID": chatId}]
+  result = callPrivateRPC("createPublicChat".prefix, payload)
