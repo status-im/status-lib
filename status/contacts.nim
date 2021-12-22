b30e7270708c56fead2e1b30d734cedc11f00e0b
Sale Djenic <aleksandardjenic@status.im>
2021-12-22 13:59:44 +0100

    update

--- status/contacts.nim
+++ status/contacts.nim
@@ -18,12 +18,6 @@ type
   ContactIdArgs* = ref object of Args
     id*: string
 
-  ContactArgs* = ref object of Args
-    contactId*: string
-
-  ContactNicknameUpdatedArgs* = ref object of ContactArgs
-    nickname*: string
-
 proc newContactModel*(events: EventEmitter): ContactModel =
     result = ContactModel()
     result.events = events
