381687ee7be10a5f28504d596642ec2744192620
Anthony Laibe <anthony@laibe.cc>
2021-12-22 14:41:13 +0100

    feat: use ens API

--- status/statusgo_backend_new/ens.nim
+++ status/statusgo_backend_new/ens.nim
@@ -3,20 +3,40 @@ import ./core, ./response_type
 
 export response_type
 
-proc resolver*(username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
-  # TODO: Use a real chain id
-  let payload = %* [1, username]
+proc resolver*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
 
   return core.callPrivateRPC("ens_resolver", payload)
 
-proc contentHash*(username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
-  # TODO: Use a real chain id
-  let payload = %* [1, username]
+proc ownerOf*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
+
+  return core.callPrivateRPC("ens_ownerOf", payload)
+
+proc contentHash*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
 
   return core.callPrivateRPC("ens_contentHash", payload)
 
-proc resourceURL*(username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
-  # TODO: Use a real chain id
-  let payload = %* [1, username]
-  return core.callPrivateRPC("ens_resourceURL", payload)
+proc publicKeyOf*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
 
+  return core.callPrivateRPC("ens_publicKeyOf", payload)
+
+proc addressOf*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
+
+  return core.callPrivateRPC("ens_addressOf", payload)
+
+proc expireAt*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
+
+  return core.callPrivateRPC("ens_expireAt", payload)
+
+proc price*(chainId: int): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId]
+  return core.callPrivateRPC("ens_price", payload)
+
+proc resourceURL*(chainId: int, username: string): RpcResponse[JsonNode] {.raises: [Exception].} =
+  let payload = %* [chainId, username]
+  return core.callPrivateRPC("ens_resourceURL", payload)
