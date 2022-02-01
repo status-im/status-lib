2be8bb55ee1915a458f3f693c51b6246af6b1ecb
Sale Djenic <aleksandardjenic@status.im>
2022-02-01 10:54:33 +0100

    refactor: privacy methods added
    - methods `changeDatabasePassword` and `getLinkPreviewWhitelist` added
    to `privacy.nim`
    - `hashPassword` moved to `utils.nim` since it's used from multiple paces
    - `getEthAccounts` added to `eth.nim`
    - `verifyAccountPassword` method updated so it returns `RpcResponse` now

--- status/statusgo_backend_new/utils.nim
+++ status/statusgo_backend_new/utils.nim
@@ -8,6 +8,5 @@ proc prefix*(methodName: string, isExt:bool = true): string =
   result = result & (if isExt: "ext_" else: "_")
   result = result & methodName
 
-
 proc hashPassword*(password: string): string =
   result = "0x" & $keccak_256.digest(password)
